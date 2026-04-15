1314db034c22bf7a370b5053e0938556a6210c98
kaushalendra-marcus <yadavkausha4a5@gmail.com>
2026-04-15 23:37:54 +0530

    user model update

--- Backend/app/db/database.py
+++ Backend/app/db/database.py
@@ -1,20 +1,37 @@
 from app.config import get_settings
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy import text
+import logging
 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 settings = get_settings()
 
 engine = create_async_engine(
     settings.DATABASE_URL, echo=True, pool_size=10, max_overflow=20
 )
 
-SessionLocal = async_sessionmaker(
+async_session_maker = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
 
 Base = declarative_base()
 
 
+async def check_db_connection():
+    async with engine.begin() as conn:
+        await conn.execute(text("SELECT 1"))
+        logger.info("Database Connected")
+
+
 async def get_db():
-    async with SessionLocal() as session:
-        yield session
+    async with async_session_maker() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
