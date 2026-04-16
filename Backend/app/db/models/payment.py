b577dea33e62c20bc75738a1fc6caa2f95e578f9
kaushalendra-marcus <yadavkausha4a5@gmail.com>
2026-04-16 18:44:25 +0530

    updating payment

---
+++ Backend/app/db/models/payment.py
@@ -0,0 +1,28 @@
+from sqlalchemy import (
+    Column,
+    DateTime,
+    Integer,
+    String,
+    UUID,
+    ForeignKey,
+    Enum,
+)
+import uuid
+from sqlalchemy.sql import func
+from app.db.database import Base
+
+
+class Payment(Base):
+    __tablename__ = "payments"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    utr = Column(String(100), unique=True, nullable=False)
+    amount = Column(Integer(50), nullable=False)
+    status = Column(
+        Enum("pending", "verified", "rejected", name="payment_status_enum"),
+        default="pending",
+        nullable=False,
+    )
+    note = Column(String(255), nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    verified_at = Column(DateTime(timezone=True), nullable=True)
