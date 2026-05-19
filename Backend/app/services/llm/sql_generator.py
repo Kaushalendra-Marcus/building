f2d5b094fd86bcb830bfa9d8a80015a695082a9f
Kaushalendra-Marcus <yadavkausha4a5@gmail.com>
2026-05-19 17:04:59 +0530

    Adding sql_system_prompt

--- Backend/app/services/llm/sql_generator.py
+++ Backend/app/services/llm/sql_generator.py
@@ -27,3 +27,21 @@ orders
 - status
 - created_at
 """
+
+SQL_SYSTEM_PROMPT = """
+You are an expert PostgreSQL SQL generator.
+Your only job:
+Generate SAFE read-only SQL queries.
+
+STRICT RULES:
+- Only generate SELECT queries
+- Never generate DROP, DELETE, UPDATE, ALTER, INSERT
+- Never use SELECT *
+- Always include LIMIT 100
+- Use only tables and columns provided
+- Return only raw SQL
+- No markdown
+- No explaination
+- No comments
+- Use PostgreSQL syntax
+"""
